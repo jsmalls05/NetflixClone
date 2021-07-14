c8ca9e0adaf6a439276041e49bea6b2d5faec312
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-07-13 20:58:50 -0400

    Add files via upload

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
                     <input type="email" placeholder="Email or Phone Number" />
                     <input type="password" placeholder="Password" />
                     <button className="loginButton">Sign In</button>
-                    <span>New to Netflix? <b>Sign up now.</b></span>
+                    <span>New to Netflix? <a href="/Register">Sign up now.</a></span>
                     <small>
                         This page is protected by Google reCAPTCHA to ensure you're not a bot. <b>Learn more</b>
                     </small>
