6502f49cd575ea550b8994f58c0b8ca9cd4026e8
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-07-13 20:56:16 -0400

    router

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
-};
+  return (
+    <BrowserRouter>
+    <div className = "App" >
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/Register" component={Register}/>        
+        <Route path="/Main" component={Home}/>
+        <Route path="/Watch" component={Watch}/>
+    </div>
+    <h2>NetFlix Clone</h2>
+    </BrowserRouter>
+  );
+}
 
 export default App;
